79266554443e52727605115cc26afc07db95f7df
guiluizmaia <68974994+guiluizmaia@users.noreply.github.com>
2020-10-14 22:42:18 -0300

    Add files via upload

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 
+import './database/conection';
+
 const app = express();
 
-app.get('/users', () =>{
+app.use(express.json());
 
+app.get('/users', (request, response) =>{
+    return response.json({message: 'Hello World'})
 });
 
 app.listen(3333);
